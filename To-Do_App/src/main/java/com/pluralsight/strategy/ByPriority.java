f8e9770dc5893fc66f97a1a83518d43643293df8
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-05 23:49:22 -0500

    ╚❮ ▪‿▪ ❯╗ By Priority class added

--- To-Do_App/src/main/java/com/pluralsight/strategy/ByPriority.java
+++ To-Do_App/src/main/java/com/pluralsight/strategy/ByPriority.java
@@ -1,4 +1,8 @@
 package com.pluralsight.strategy;
+import com.pluralsight.model.Task;
+import java.util.Comparator;
 
-public class ByPriority {
+public class ByPriority implements TaskSortStrategy {
+    public Comparator<Task> getComparator() { return Comparator.comparing(Task::getPriority).reversed(); }
+    public String getName() { return "By Priority"; }
 }
