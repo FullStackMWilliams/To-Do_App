a43e25656e3b59e7acdf0c8de0dd1df4701a8a13
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-05 23:44:28 -0500

    (ㆆ__ㆆ) By Due Date class added

--- To-Do_App/src/main/java/com/pluralsight/strategy/ByDueDate.java
+++ To-Do_App/src/main/java/com/pluralsight/strategy/ByDueDate.java
@@ -1,4 +1,9 @@
 package com.pluralsight.strategy;
+import com.pluralsight.model.Task;
+import java.util.Comparator;
 
-public class ByDueDate {
+public class ByDueDate implements TaskSortStrategy {
+    public Comparator<Task> getComparator() { return Comparator.comparing(Task::getDueAt, Comparator.nullsLast(Comparator.naturalOrder())); }
+    public String getName() { return "By Due Date"; }
 }
+
