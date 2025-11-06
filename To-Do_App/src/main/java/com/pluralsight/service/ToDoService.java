1e4f437e8f429631f99f7b7d253acd42a5c9e1f8
FullStackMWilliams <markuswilliams99@icloud.com>
2025-11-05 23:27:04 -0500

    (º‒º) To do service Interface

--- To-Do_App/src/main/java/com/pluralsight/service/ToDoService.java
+++ To-Do_App/src/main/java/com/pluralsight/service/ToDoService.java
@@ -1,4 +1,17 @@
 package com.pluralsight.service;
 
-public class ToDoService {
+import com.pluralsight.model.*;
+import com.pluralsight.strategy.TaskSortStrategy;
+import java.util.List;
+import java.util.UUID;
+
+public interface ToDoService {
+    TaskList createList(String name);
+    List<TaskList> allLists();
+    void deleteList(UUID listId);
+
+    void addTask(UUID listId, String title, String description, Priority p);
+    List<Task> getTasks(UUID listId, TaskSortStrategy strategy);
+    void completeTask(UUID listId, UUID taskId);
 }
+
